ff344a94d3341287a2697860226fc5c992354f09
mcalcagno <calcagno92@gmail.com>
2016-09-12 20:12:20 -0300

    commit

--- Dispatcher/src/main/java/middleware/grupo01/listener/OrderMessageListener.java
+++ Dispatcher/src/main/java/middleware/grupo01/listener/OrderMessageListener.java
@@ -8,12 +8,13 @@ import javax.jms.TextMessage;
 public class OrderMessageListener implements MessageListener {
 
 	public void onMessage(Message message) {
-		try {
+		//try {
+			@SuppressWarnings(value="unused")
 			TextMessage msg = (TextMessage) message;
-			System.out.println("Consumed message: " + msg.getText());
-		} catch (JMSException e) {
+			System.out.println("Input Consumed message: " );
+		/*} catch (JMSException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 
 }
