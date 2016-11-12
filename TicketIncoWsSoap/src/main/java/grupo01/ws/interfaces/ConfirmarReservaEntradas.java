40ff7b79655ea47f13b8c5719c3c5a54d3336975
clvf91 <cvega030191@gmail.com>
2016-11-11 22:47:07 -0300

    confirmacion

--- TicketIncoWsSoap/src/main/java/grupo01/ws/interfaces/ConfirmarReservaEntradas.java
+++ TicketIncoWsSoap/src/main/java/grupo01/ws/interfaces/ConfirmarReservaEntradas.java
@@ -2,7 +2,9 @@ package grupo01.ws.interfaces;
 
 import java.util.Date;
 
+import grupo01.ws.data.EntradaData;
+
 public interface ConfirmarReservaEntradas {
 
-	Long confirmarReservaVenta(Long idReserva, Long idMedioPago, String nroTarjeta, Date fechaVenc, Integer digitoVerificador);
+	EntradaData confirmarReservaVenta(Long idReserva, Long idMedioPago, String nroTarjeta, Date fechaVenc, Integer digitoVerificador);
 }
